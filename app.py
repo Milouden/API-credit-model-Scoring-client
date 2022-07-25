b238fff4bf900b2cdaab122c40498372f6259778
Milouden <76050842+Milouden@users.noreply.github.com>
2022-07-25 11:49:53 -0400

    Add files via upload

--- app.py
+++ app.py
@@ -96,5 +96,5 @@ if __name__ == "__main__":
     ##app.run(host = '127.0.0.1', port = 5000, debug = True)
     #http://127.0.0.1:5000
     ##serve(app, host="0.0.0.0", port=8080)
-    #app.run(debug=True)
+    app.run(debug=True)
     ##app.run()
