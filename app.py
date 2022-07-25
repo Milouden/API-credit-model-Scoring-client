7cd0bdf704eafeef2e198fa00665a651274fe3bd
Milouden <76050842+Milouden@users.noreply.github.com>
2022-07-25 11:36:21 -0400

    Add files via upload

--- app.py
+++ app.py
@@ -88,7 +88,7 @@ if __name__ == "__main__":
     #import myapp
     #waitress.serve(myapp.wsgifunc, port=8041, url_scheme='https')
     from waitress import serve
-    serve(app, host="0.0.0.0", port=8080)
+    serve(app, host="127.0.0.1", port=8080)
     ## uvicorn.run(app = '127.0.0.1', port = 8000, debug = True)
     ##app.run(host = 'localhost', port = 8088, debug = True)
     ## from waitress import serve
