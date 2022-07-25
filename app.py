fcc0b22b458c94dcb4698991fd7fb5907c60d5f8
Milouden <76050842+Milouden@users.noreply.github.com>
2022-07-25 10:50:17 -0400

    Add files via upload

--- app.py
+++ app.py
@@ -96,8 +96,10 @@ def prediction_credit(id_client):
 
 
 if __name__ == "__main__":
-    from waitress import serve
-    serve(app, host="0.0.0.0", port=8080)
+    import myapp
+    waitress.serve(myapp.wsgifunc, port=8041, url_scheme='https')
+    #from waitress import serve
+    #serve(app, host="0.0.0.0", port=8080)
     ## uvicorn.run(app = '127.0.0.1', port = 8000, debug = True)
     ##app.run(host = 'localhost', port = 8088, debug = True)
     ## from waitress import serve
